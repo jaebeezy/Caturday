009304261860de9fd27eb3469f33607e477b73fe
jaebeezy <jaeyoungnsb@gmail.com>
2022-02-01 21:53:18 -0500

    home screen. trying again?

--- Caturday/VIew/HomeView.swift
+++ Caturday/VIew/HomeView.swift
@@ -9,59 +9,26 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var date = Date()
+    
     let fact: String
-    
-    // formating time to add seconds
-    var timeFormat: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "hh:mm:ss a"
-        return formatter
-    }
-    
-    // transform date obj to string
-    func timeString(_ date: Date) -> String {
-        let time = timeFormat.string(from: date)
-        return time
-    }
-    
-    // function to live update timer
-    var updateTimer: Timer {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            self.date = Date()
-        }
-    }
-    
-    /// MARK:
-    /// need to animate the home page UI
-    /// i want an animated time and date
-    /// and then adding like a random cat fact. and maybe pressing a button to fetch a new cat fact.
     
     var body: some View {
         NavigationView {
-            VStack {
-                VStack {
-                    Text("Today is")
-                    Text(date, style: .date)
+            
+            if fact.isEmpty {
+                ProgressView()
+            } else {
+                Section {
+                    TileView(fact: fact)
                 }
-                
-                VStack {
-                    Text("The current time is")
-                    Text(timeString(date))
-                        .onAppear {
-                            let _ = self.updateTimer
-                        }
-                }
-                                
-                Text("And it is Caturday")
-                
-                Divider()
-                
-                Text(fact)
+                .navigationTitle("Home")
             }
-            .navigationTitle("Welcome")
         }
-        
     }
 }
 
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(fact: "hello")
+    }
+}
